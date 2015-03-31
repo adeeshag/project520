892547f83815e16b609495acfc98ef40cf32130f
adeeshag <adeeshag@aol.com>
2015-03-30 22:16:12 -0400

    Temporary backup. Written much more logic for different states of FSM.

--- chnge_in_y_integration/filter_yVal.v
+++ chnge_in_y_integration/filter_yVal.v
@@ -46,6 +46,7 @@ parameter
 /* Wires and regs */
 reg [2:0] current_state, next_state;
 reg [47:0] op_yVal1,op_yVal2;
+reg [47:0] temp_yVal;
 reg [15:0] op_y_row;
 reg op_EX_EN;
 
@@ -75,6 +76,7 @@ begin
    op_yVal2 = 48'bz;
    op_EX_EN = 1'b0;
    op_y_row = 16'bz;
+   temp_yVal = 48'b0;
    // Set next state
    //
    end
@@ -84,20 +86,122 @@ begin
    op_yVal2 = 48'bz;
    op_EX_EN = 1'b0;
    op_y_row = chng_row;
+   temp_yVal = 48'b0;
    // Set next state
 
    end
    s2: begin
    //Set outputs
-   op_yVal1 = 48'bz;
+   //Calc y Diag1
+   if(&(ymem_data[255:253])) // all bits are high
+   begin
+      op_yVal1 = ymem_data[239:192];
+      //Now check the rest for the diag element
+      if((ymem_data[191:189])==(chng_col)) 
+      begin
+         temp_yVal = ymem_data[175:128];
+      end
+      else if((ymem_data[127:125])==(chng_col)) 
+      begin
+         temp_yVal = ymem_data[111:64];
+      end
+      else if((ymem_data[63:61])==(chng_col)) 
+      begin
+         temp_yVal = ymem_data[47:0];
+      end
+      else
+         temp_yVal = 48'b0;
+      //
+   end
+   else if(&(ymem_data[191:189])) 
+   begin
+      op_yVal1 = ymem_data[175:128];
+
+      //Now check the rest for the diag element
+      if((ymem_data[127:125])==(chng_col)) 
+      begin
+         temp_yVal = ymem_data[111:64];
+      end
+      else if((ymem_data[63:61])==(chng_col)) 
+      begin
+         temp_yVal = ymem_data[47:0];
+      end
+      else
+         temp_yVal = 48'b0;
+      //
+   end
+   else if(&(ymem_data[127:125])) 
+   begin
+      op_yVal1 = ymem_data[111:64];
+
+      //Now check the rest for the diag element
+      if((ymem_data[63:61])==(chng_col)) 
+      begin
+         temp_yVal = ymem_data[47:0];
+      end
+      else
+         temp_yVal = 48'b0;
+      //
+   end
+   else if(&(ymem_data[63:61])) 
+   begin
+      op_yVal1 = ymem_data[47:0];
+   end
+   // 
    op_yVal2 = {chng_real,chng_img};
    op_EX_EN = 1'b1;
    op_y_row = chng_col;
    // Set next state
 
-   end
+   end// s2
+   s3: begin
 
-end
+   op_yVal2 = {chng_real,chng_img};
+   op_y_row = 15'bz; // don't need this
+   
+   //
+   if(|temp_yVal)
+   begin
+      op_yVal1 = temp_yVal;
+      op_EX_EN = 1'b1;
+   end// if
+   else
+   begin
+      //Now check for the diag element
+      if((ymem_data[255:240])==(chng_col))) 
+      begin
+         op_yVal1 = ymem_data[239:192];
+         op_EX_EN = 1'b1;
+      end
+      else if((ymem_data[191:189])==(chng_col)) 
+      begin
+         op_yVal1 = ymem_data[175:128];
+         op_EX_EN = 1'b1;
+      end
+      else if((ymem_data[127:125])==(chng_col)) 
+      begin
+         op_yVal1 = ymem_data[111:64];
+         op_EX_EN = 1'b1;
+      end
+      else if((ymem_data[63:61])==(chng_col)) 
+      begin
+         op_yVal1 = ymem_data[47:0];
+         op_EX_EN = 1'b1;
+      end
+      else
+         op_yVal1 = 48'b0;
+         op_EX_EN = 1'b0;
+      //
+      end
+
+   end// if-else
+
+   // Set next state
+
+   end//s3
+
+
+end // always@(*)
 
 
 
